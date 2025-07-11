0ff29c1c5fa2069eaae18a191fe81632c416e2c8
hanuman808 <bavanehanuman436@gmail.com>
2025-07-11 10:30:35 +0530

    .

--- src/DayTweleve/Demo.java
+++ src/DayTweleve/Demo.java
@@ -6,10 +6,23 @@ public class Demo {
 		Thread myThread = new ThreadLifeCycleDemo();
 		System.out.println("Before Runnable State Thread is Alive");
 		myThread.isAlive();
-		try {Thread.sleep(450);
-	}catch(InterruptedException e) {
-		System.err.println(e.getMessage());
+		
+		myThread.start();
+		
+		try 
+		{
+			Thread.sleep(450);
+	
+		}
+		
+		catch(InterruptedException e) 
+		{
+			System.err.println(e.getMessage());
+		}
+
+		System.out.println("After Completion Execution, it is alive or not ? " + myThread.isAlive());
+
+		
 	}
-System.out.println("After Completion Execution ,it "+myThread.isAlive());
-}
+	
 }
--- src/DayTweleve/ThreadLifeCycleDemo.java
+++ src/DayTweleve/ThreadLifeCycleDemo.java
@@ -1,21 +1,32 @@
 package DayTweleve;
 
 public class ThreadLifeCycleDemo extends Thread {
-	public void run() { 
-		System.out.println(" in side run () thread  is alive or not "+this.isAlive());
-		int num=0;
-		while(num<4) {
+	
+	public void run() 
+	{ 
+		
+		System.out.println("Inside run(), thread is alive or not ? \n" + this.isAlive());
+		int num = 0;
+		
+		while(num<4) 
+		{
 			num++;
-			System.out.println("NUm "+num);
-		}try {
-			sleep(1000);
-			System.out.println("in npt runnabe state ,thread is alive"+this.isAlive());
-			
-		}catch(InterruptedException e) {
-			System.err.println("Thread is interrupted "+ e.getMessage());
+			System.out.println("Num " + num);
 		}
 		
+		try 
+		{
+			sleep(1000);
+			System.out.println("In not runnabe state, thread is alive or not ? \n" + this.isAlive());
+		}
+		
+		catch(InterruptedException e) 
+		{
+			System.err.println("Thread is interrupted " + e.getMessage());
+		}
+	
 	}
 
+		
 	
 }
